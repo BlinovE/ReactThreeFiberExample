1c1bdc9166bf51af55a155394ed6c1b381862bf0
BlinovE <95091610+BlinovE@users.noreply.github.com>
2022-07-30 22:41:39 +0300

    Add Camera Helper

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import ReactDOM from 'react-dom';
-import { Canvas } from '@react-three/fiber';
+import ReactDOM from "react-dom";
+import { Canvas } from "@react-three/fiber";
+import { PerspectiveCamera } from "three";
+
+function CameraHelper() {
+  const camera = new PerspectiveCamera(60, 1, 1, 2);
+  return (
+    <group position={[0, 0, 2]}>
+      <cameraHelper args={[camera]} />
+    </group>
+  );
+}
 
 function App() {
   return (
     <div id="canvas-container">
-      <Canvas/>
+      <Canvas camera={{ fov: 60, near: 1, far: 3, position: [0, 0, 2] }}>
+        <CameraHelper />
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[1, 2, 5]} />
+        <mesh
+          visible
+          position={[0, 0, 0]}
+          rotation={[Math.PI / 4, Math.PI / 7, Math.PI / 6]}
+        >
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
     </div>
   );
 }
