5f92981f7605873288241f2107b2cdb26cd35d82
BlinovE <95091610+BlinovE@users.noreply.github.com>
2022-07-30 23:41:01 +0300

    Events
    работа с событиями <mesh/>

--- src/components/MyAnimatedBox.jsx
+++ src/components/MyAnimatedBox.jsx
@@ -1,14 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-function MyAnimatedBox(){
-    const [active, setActive] = useState(false);
+function MyAnimatedBox() {
+  const [active, setActive] = useState(false);
 
-    return (
-        <mesh scale={ active ? 1.5:1 } onClick={()=>setActive(!active)}>
-            <boxGeometry />
-            <meshPhongMaterial color="royalblue" />
-        </mesh>
-    )
+  return (
+    <mesh
+      scale={active ? 1.5 : 1}
+      onClick={() => setActive(!active)}
+      onDoubleClick={(e)=>{
+        console.log("DblClick")
+      }}
+      onPointerOver={(e) => {
+        console.log("Over");
+      }}
+      onPointerOut={(e)=>{
+        console.log("Out")
+      }}
+      onPointerEnter={(e)=>{
+        console.log("Enter")
+      }}
+      onPointerLeave={(e)=>{
+        console.log("Leave")
+      }}
+      onPointerMissed={(e)=>{
+        console.log("Missed")
+      }}
+      onPointerMove={(e)=>{
+        console.log("move")
+      }}
+      onPointerUp={(e)=>{
+        console.log("Up")
+      }}
+      onPointerDown={(e)=>{
+        console.log("Down")
+      }}
+      onWheel={(e)=>{
+        console.log("Wheel")
+      }}
+      onContextMenu={(e)=>{
+        console.log("Menu")
+      }}
+    >
+      <boxGeometry />
+      <meshPhongMaterial color="royalblue" />
+    </mesh>
+  );
 }
 
-export default MyAnimatedBox
+export default MyAnimatedBox;
