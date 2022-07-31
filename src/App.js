b19805d99e07bf831cf5db43e28437ac7a58042d
BlinovE <95091610+BlinovE@users.noreply.github.com>
2022-07-31 21:40:59 +0300

    OrbitControl
    Добавил манипулятор OrbitControl

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React, { useRef } from 'react';
-import { Canvas, useFrame } from "@react-three/fiber";
+import React, { useRef, useEffect } from 'react';
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import {MyAnimatedBox, MySpringBox} from "./components/MyAnimatedBox";
+
+const CameraController = ()=>{
+  const { camera, gl } = useThree();
+  useEffect(() => {
+    const controls = new OrbitControls(camera, gl.domElement)
+
+    controls.minDistance = 3;
+    controls.maxDistance = 20;
+    return () => {
+      controls.dispose();
+    };
+  }, [camera, gl]);
+  return null;
+}
 
 function AnimateRotateXYZMesh(){
   const animateYMesh = useRef();
@@ -16,13 +31,20 @@ function AnimateRotateXYZMesh(){
 
 
 function App() {
+
+  const cubes = []
+
+  for(let i=0;i<10;i++){
+    cubes.push(<MySpringBox key={i} position={[i*2,0,0]}/>);
+  }
   
   return (
     <div id="canvas-container">
-      <Canvas camera={{ fov: 60, near: 1, far: 3, position: [0, 0, 2] }}>
+      <Canvas orthographic camera={{ left:-3, right:3, top: 3, bottom:-3, near: 1, far: 10, zoom:30}}>
+        <CameraController/>
         <ambientLight intensity={0.1} />
         <directionalLight />
-        <MySpringBox/>
+        {cubes}
       </Canvas>
     </div>
   );
